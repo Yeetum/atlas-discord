169dd7ce4ca876101e16aaf42fb4fc2173f0580c
tobalo <nexxis817@gmail.com>
2021-06-07 11:06:42 -0500

    OCI SDK configured and ready to retreive object storage

--- bot.js
+++ bot.js
@@ -1,45 +1,98 @@
+/**
+ * This bot is designed for the Synergy by Yeetum Discord community. Built and managed by Yeetum Technologies.
+ * 
+*/
 const Discord = require('discord.js');
 require('dotenv').config();
-const client = new Discord.Client();
 const fs = require('fs');
-const http = require('https');
+const ociOS = require("oci-objectstorage");
+const common = require("oci-common");
+const path = require('path');
 // Set command prefix for additional capabilities
 const PREFIX = '?yeet';
-
+const discordClient = new Discord.Client();
 
 /**
+ * Discord init w/ ATLAS bot
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
  * received from Discord
 */
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`);
+  });
+
+/*
+* Note: there is a 2GB for 64-bit machine and 1GB for 32-bit machine buffer limitation from the NodeJS V8 Engine
+* Cannot upload file size greater than the limit
+*/
+
+// Init OCI Object Storage Client
+// Using personal configuration
+const provider = new common.ConfigFileAuthenticationDetailsProvider();
+const ociClient = new ociOS.ObjectStorageClient({
+    authenticationDetailsProvider: provider
 });
 
+(async () => {
+    try {
+        console.log("Getting OCI OS Namespace...");
+        const request = {};
+        const nsResponse = await ociClient.getNamespace(request);
+        const namespace = nsResponse.value;
+        console.log('OCI OS Namespace:', namespace);
 
-client.on('message', message => {
+        const bucketName = 'fum_reports';
 
-    // Always yeet... just cause
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+        today = yyyy + '-' + mm + '-' + '06';
+        console.log('Date time:', today);
+
+        const objectName = 'crypto.signals.' + today + '.json';
+        console.log('Object Name:', objectName);
+
+        const getObjectRequest = {
+            objectName: objectName,
+            bucketName: bucketName,
+            namespaceName: namespace
+        };
+
+        const getObjectResponse = await ociClient.getObject(getObjectRequest);
+        console.log("Get Object executed successfully.");
+        console.log("Object Data:", getObjectResponse.value);
+
+    } catch (e) {
+        console.log("Error:", e);
+    }
+})();
+
+
+// Always respond to yeet... just cause
+discordClient.on('message', message => {
+
     if (message.content === 'yeet') {
         message.reply('counter-yeet');
     }
+});
 
-	client.on('message', message => {
-        if (message.content === `${PREFIX} crypto`) {
-            //const obj = http.get('https://objectstorage.us-phoenix-1.oraclecloud.com/p/3VPOg9nO3fcgvNsVwztdq7tfvJbl2Nnp30KCD2T4y8bk3Wp5iLq_defZ9rjMQ8hB/n/ax8pmzkbraag/b/fum_reports/o/crypto.signals.2021-06-05.json');
-            //message.channel.send(obj);
-            message.channel.send('Crypto Signals Automation being developed');
-        } else if (message.content === `${PREFIX} stocks`) {
-            message.channel.send('Stock Signals Automation Dev');
-        } else if (message.content === `${PREFIX} server`) {
-            message.channel.send(`Server name: ${message.guild.name}\nTotal Yeeters: ${message.guild.memberCount}`);
-        } else if (message.content === `${PREFIX} user`) {
-            message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-        }
-    });
+discordClient.on('message', message => {
+    if (message.content === `${PREFIX} crypto`) {
+        //const obj = http.get('https://objectstorage.us-phoenix-1.oraclecloud.com/p/3VPOg9nO3fcgvNsVwztdq7tfvJbl2Nnp30KCD2T4y8bk3Wp5iLq_defZ9rjMQ8hB/n/ax8pmzkbraag/b/fum_reports/o/crypto.signals.2021-06-05.json');
+        //message.channel.send(obj);
+        message.channel.send('Crypto Signals Automation being developed');
+    } else if (message.content === `${PREFIX} stocks`) {
+        message.channel.send('Stock Signals Automation Dev');
+    } else if (message.content === `${PREFIX} server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal Yeeters: ${message.guild.memberCount}`);
+    } else if (message.content === `${PREFIX} user`) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    }
 });
 
 // If PAR on Object Storage permits pulling
-client.on('message', message => {
+discordClient.on('message', message => {
     // If the message is '!rip'
     if (message.content === '!signals') {
       // Create the attachment using MessageAttachment
@@ -52,7 +105,7 @@ client.on('message', message => {
 
 
 // If report is local to discord host
-client.on('message', message => {
+discordClient.on('message', message => {
     // If the message is '!memes'
     if (message.content === '!memes') {
       // Get the buffer from the 'memes.txt', assuming that the file exists
@@ -77,4 +130,4 @@ const hook = new Discord.WebhookClient('843674945011318826', '-Ee7ddMeHX8MvZopsv
 // Send a report to webhook
 //hook.send('ATLAS n8n webhook');
 
-client.login(process.env.BOT_TOKEN);
+discordClient.login(process.env.BOT_TOKEN);
