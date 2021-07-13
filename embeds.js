5aa44339570b60e0ee5e80a96fe4d13075bfe4c5
tobalo <nexxis817@gmail.com>
2021-07-13 07:31:00 -0500

    leveraged .env vars for security

--- embeds.js
+++ embeds.js
@@ -1,11 +1,12 @@
 const config = require('./config');
 const Discord = require('discord.js');
+require('dotenv').config();
 
-const aiPng = config.aiPngURI;
-const yeetumPNG = config.yeetumWhiteURI;
+const aiPng = process.env.aiPngURI || config.aiPngURI;
+const yeetumPNG = process.env.yeetumWhiteURI || config.yeetumWhiteURI;
 const disclaimer = 'Yeetum Investment Group cannot warranty the expressions and suggestions of the contents, as well as its accuracy. In addition, to the extent permitted by the law, Yeetum Investment Group shall not be responsible for any losses and/or damages due to the usage of the information on our website.\nBy using our services, you hereby consent to our disclaimer and agree to its terms. For these external sites, Yeetum Investment Group cannot be held liable for the availability of, or the content located on or through it.  Plus, any losses or damages occurred from using these contents or the internet generally.';
 const REPORTFILE = 'crypto.signals.2021-07-12.json';
-const reportURI = config.bucketReportURI + REPORTFILE;
+const reportURI = process.env.reportURI + REPORTFILE;
 
 const cryptoAttachment = new Discord.MessageAttachment(reportURI);
 const cryptoSignalEmbed = new Discord.MessageEmbed()
