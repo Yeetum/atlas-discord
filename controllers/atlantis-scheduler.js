fafd95da3110a20d1fb7c06e35c630fc83ea708e
tobalo <nexxis817@gmail.com>
2021-07-13 09:15:04 -0500

    Update atlantis-scheduler.js

--- controllers/atlantis-scheduler.js
+++ controllers/atlantis-scheduler.js
@@ -5,8 +5,10 @@ const schedule = require('node-schedule');
 module.exports = discordClient => {
     const rule = new schedule.RecurrenceRule();
     console.log("atlantis-scheduler activated");
+    rule.hour = 16;
+    rule.minute = 20;
 
-    const atlantisClubJob = schedule.scheduleJob({hour: 16, minute: 20, dayOfWeek: 0}, function(){
+    const atlantisClubJob = schedule.scheduleJob(rule, function(){
         
         discordClient.channels.get('840265345214578708').send(embeds.cryptoSignalEmbed);
         discordClient.channels.get('850086242926198794').send(embeds.stockSignalEmbed);
