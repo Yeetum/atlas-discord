027e7944ddd8b0d29376281363067062958fc406
tobalo <nexxis817@gmail.com>
2022-03-30 14:01:20 -0500

    added private eden club disc

--- controllers/atlantis-scheduler.js
+++ controllers/atlantis-scheduler.js
@@ -24,6 +24,8 @@ module.exports = discordClient => {
             let cryptoAttachmentURI = prepAttach.prepCryptoReportName('crypto');
             discordClient.channels.cache.get('840265345214578708').send(embeds.cryptoDailyEmbed);
             discordClient.channels.cache.get('840265345214578708').send(new Discord.MessageAttachment(cryptoAttachmentURI));
+            discordClient.channels.cache.get('958757453091176505').send(embeds.cryptoDailyEmbed);
+            discordClient.channels.cache.get('958757453091176505').send(new Discord.MessageAttachment(cryptoAttachmentURI));
             console.log("crypto job successful...");
         } 
         catch (e) {
@@ -36,6 +38,8 @@ module.exports = discordClient => {
             let stockAttachmentURI = prepAttach.prepStockReportName('stocks');
             discordClient.channels.cache.get('850086242926198794').send(embeds.stockDailyReportEmbed);
             discordClient.channels.cache.get('850086242926198794').send(new Discord.MessageAttachment(stockAttachmentURI));
+            discordClient.channels.cache.get('958757453091176506').send(embeds.stockDailyReportEmbed);
+            discordClient.channels.cache.get('958757453091176506').send(new Discord.MessageAttachment(stockAttachmentURI));
             console.log("stock job successful...");
         } 
         catch (e) {
