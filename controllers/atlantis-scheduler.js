d79f517b47150ac052b689f83ccdb287c11769b6
tobalo <nexxis817@gmail.com>
2022-05-12 13:40:50 +0000

    remove synergy channels, exclusive to eden club discord

--- controllers/atlantis-scheduler.js
+++ controllers/atlantis-scheduler.js
@@ -22,8 +22,6 @@ module.exports = discordClient => {
     let atlantisClubCryptoJob = schedule.scheduleJob(cryptoRule, function(){
         try{
             let cryptoAttachmentURI = prepAttach.prepCryptoReportName('crypto');
-            discordClient.channels.cache.get('840265345214578708').send(embeds.cryptoDailyEmbed);
-            discordClient.channels.cache.get('840265345214578708').send(new Discord.MessageAttachment(cryptoAttachmentURI));
             discordClient.channels.cache.get('958757453091176505').send(embeds.cryptoDailyEmbed);
             discordClient.channels.cache.get('958757453091176505').send(new Discord.MessageAttachment(cryptoAttachmentURI));
             console.log("crypto job successful...");
@@ -36,8 +34,6 @@ module.exports = discordClient => {
     let atlantisClubStockJob = schedule.scheduleJob(stockRule, function(){
         try {
             let stockAttachmentURI = prepAttach.prepStockReportName('stocks');
-            discordClient.channels.cache.get('850086242926198794').send(embeds.stockDailyReportEmbed);
-            discordClient.channels.cache.get('850086242926198794').send(new Discord.MessageAttachment(stockAttachmentURI));
             discordClient.channels.cache.get('958757453091176506').send(embeds.stockDailyReportEmbed);
             discordClient.channels.cache.get('958757453091176506').send(new Discord.MessageAttachment(stockAttachmentURI));
             console.log("stock job successful...");
